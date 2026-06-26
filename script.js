1428c2defa6f5d229920b4be1b54d745488b3180
Mohit Negi <mohitnegiworks@gmail.com>
2026-06-26 18:58:42 +0530

    prevent default learned or used

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const button = document.getElementById('TaskButton');
 button.addEventListener('click',function(e){
     e.preventDefault();
     const taskText = document.getElementById('action').value;
+    if(taskText.trim()===''){
+        alert("Please Enter your Tasks.");
+        return;
+    }
     const newLi = document.createElement('li');
     newLi.textContent = taskText;
     
